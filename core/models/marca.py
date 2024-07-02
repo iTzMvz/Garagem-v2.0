0d8eef5a9e36e72c5f014be934d63fc1b923c92b
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-07-02 09:15:20 -0300

    Correção do nome da marca e adição de itens nas tabelas

--- core/models/marca.py
+++ core/models/marca.py
@@ -6,4 +6,4 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.id} - {self.nome}'
+        return f'{self.id} - {self.nome.upper()}'
